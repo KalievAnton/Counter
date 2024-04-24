d54a64da3c90f12d1a66296b39e5ea5a0ae9ca3e
Kaliev Anton <antonfrukt@rambler.ru>
2024-04-24 21:55:46 +0300

    Базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var titleButton: UIButton!
+    
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureAppearance()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        labelText.text = "0"
+        
+        titleButton.setTitle("Счетчик", for: .normal)
+        titleButton.backgroundColor = .lightGray
+        titleButton.tintColor = .black
+        titleButton.layer.cornerRadius = 10
+    }
+    
+    private func configureAppearance() {
+        titleButton.addTarget(self, action: #selector (titleButtonAction), for: .touchUpInside)
+    }
+    
+    @objc func titleButtonAction() {
+        counter += 1
+        labelText.text = "Значение счётчика: \(counter)"
+    }
 }
 
